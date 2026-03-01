73891b6bb5a2e2580771ab6d5c87eb64ffcf9d2e
getItemFromBlock <quentin.lpn25@gmail.com>
2026-03-01 21:41:29 +0100

    Fixed procedural meshes ignoring indices

--- Engine/src/Resource/Mesh.cpp
+++ Engine/src/Resource/Mesh.cpp
@@ -15,7 +15,7 @@ void Mesh::CreateFrom(float *vertices, uint32_t verticeCount, uint32_t *indices,
 {
     p_isLoaded = false;
     p_sendToGPU = true;
-    ASSERT(vertices != nullptr && verticeCount > 0 && indices != nullptr && indiceCount > 0);
+    ASSERT(vertices != nullptr && verticeCount > 0 && (indiceCount == 0 || indices != nullptr));
     ASSERT(indiceCount % 3 == 0);
 
     const uint32_t verticeSize = (isWeighted ? sizeof(WeightedVertex) : sizeof(Vertex)) / sizeof(float);
@@ -23,6 +23,12 @@ void Mesh::CreateFrom(float *vertices, uint32_t verticeCount, uint32_t *indices,
     m_vertices.resize(verticeCount * verticeSize);
     std::copy(vertices, vertices + (verticeCount * verticeSize), m_vertices.data());
     m_isWeighted = isWeighted;
+
+    if (indiceCount)
+    {
+        m_indices.resize(indiceCount);
+        std::copy(indices, indices + indiceCount, m_indices.data());
+    }
 
     SendToGPU(Engine::Get()->GetRenderer());
     p_isLoaded = true;
